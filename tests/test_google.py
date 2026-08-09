532dd8ac81814f1583bb3be97b12286087eaec52
nirgunav <nirgunavalvekar@gmail.com>
2026-08-09 19:45:03 +0530

    Fix Google test for CI Chrome

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
+import pytest
 
 
-def test_open_google():
-    driver = webdriver.Chrome()
-
+@pytest.mark.smoke
+def test_open_google(driver):
     driver.get("https://www.google.com")
 
     assert "Google" in driver.title
-
-    driver.quit()
